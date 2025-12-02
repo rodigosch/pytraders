013b6281443d6a38baee555d77fe6d8b83ae5476
Rodrigo Schneider <rodrigoschneider@tjrs.jus.br>
2025-12-02 11:12:26 -0300

    Refatoração parte 1, correção 13

--- pytraders/carteira.py
+++ pytraders/carteira.py
@@ -22,8 +22,7 @@ class Carteira:
     def setup_backtest(self, capital_inicial, diversificacao_maxima, reinvestir_lucros, taxa_custo_operacional, pregoes, filtrar_operacao_curva_capital=False):
         self.filtrar_operacao_curva_capital = filtrar_operacao_curva_capital
         self.book_execucao = TradingBook(self.indice_b3, self.data_inicio, self.data_fim, capital_inicial, diversificacao_maxima, reinvestir_lucros, taxa_custo_operacional, pregoes, filtrar_operacao_curva_capital)
-        if (self.filtrar_operacao_curva_capital):
-            self.book_referencia = TradingBook(self.indice_b3, self.data_inicio, self.data_fim, capital_inicial, diversificacao_maxima, reinvestir_lucros, taxa_custo_operacional, pregoes, filtrar_operacao_curva_capital)
+        self.book_referencia = TradingBook(self.indice_b3, self.data_inicio, self.data_fim, capital_inicial, diversificacao_maxima, reinvestir_lucros, taxa_custo_operacional, pregoes, filtrar_operacao_curva_capital)
 
 
     # Recebe um índice B3 e retorna um dataframe dos ativos que o compõe
@@ -105,76 +104,46 @@ class Carteira:
 
     def atualizarPatrimonio(self, data, operacao, valor):
         self.book_execucao.atualizarPatrimonio(data, operacao, valor)
-        if (self.filtrar_operacao_curva_capital):
-            self.book_referencia.atualizarPatrimonio(data, operacao, valor)
+        self.book_referencia.atualizarPatrimonio(data, operacao, valor)
 
     def getPregoes(self):
-        if (self.filtrar_operacao_curva_capital):
-            return self.book_referencia.pregoes
-        else:
-            return self.book_execucao.pregoes
+        return self.book_referencia.pregoes
 
     def temPosicaoAberta(self, ativo):
-        if (self.filtrar_operacao_curva_capital):
-            return self.book_referencia.temPosicaoAberta(ativo)
-        else:
-            return self.book_execucao.temPosicaoAberta(ativo)
+        return (self.book_referencia.temPosicaoAberta(ativo) or self.book_execucao.temPosicaoAberta(ativo))
         
     def abrirPosicao(self, dataEntrada, ativo, tipo, volume, precoEntrada, forcaRelativa, stopLoss=np.nan, stopGain=np.nan):
-        if (self.filtrar_operacao_curva_capital):
-            self.book_referencia.abrirPosicao(dataEntrada, ativo, tipo, volume, precoEntrada, forcaRelativa, stopLoss, stopGain)
+        self.book_referencia.abrirPosicao(dataEntrada, ativo, tipo, volume, precoEntrada, forcaRelativa, stopLoss, stopGain)
         if ((self.filtrar_operacao_curva_capital and self.book_referencia.curva_capital_acima_media_movel()) or not self.filtrar_operacao_curva_capital):
             self.book_execucao.abrirPosicao(dataEntrada, ativo, tipo, volume, precoEntrada, forcaRelativa, stopLoss, stopGain)
 
     def fecharPosicao(self, dataSaida, ativo, precoSaida):
+        self.book_referencia.fecharPosicao(dataSaida, ativo, precoSaida)
         if (self.book_execucao.temPosicaoAberta(ativo)):
             self.book_execucao.fecharPosicao(dataSaida, ativo, precoSaida)
-        if (self.filtrar_operacao_curva_capital):
-            self.book_referencia.fecharPosicao(dataSaida, ativo, precoSaida)
 
     def getStopLossPosicaoAberta(self, ativo):
-        if (self.filtrar_operacao_curva_capital):
-            return self.book_referencia.getStopLossPosicaoAberta(ativo)
-        else:
-            return self.book_execucao.getStopLossPosicaoAberta(ativo)
+        return self.book_referencia.getStopLossPosicaoAberta(ativo)
         
     def getStopGainPosicaoAberta(self, ativo):
-        if (self.filtrar_operacao_curva_capital):
-            return self.book_referencia.getStopGainPosicaoAberta(ativo)
-        else:
-            return self.book_execucao.getStopGainPosicaoAberta(ativo)
+        return self.book_referencia.getStopGainPosicaoAberta(ativo)
             
     def getTipoPosicaoAberta(self, ativo):
-        if (self.filtrar_operacao_curva_capital):
-            return self.book_referencia.getTipoPosicaoAberta(ativo)
-        else:
-            return self.book_execucao.getTipoPosicaoAberta(ativo)
+        return self.book_referencia.getTipoPosicaoAberta(ativo)
         
     def getVolumeOperacao(self, preco):
-        if (self.filtrar_operacao_curva_capital):
-            return self.book_referencia.getVolumeOperacao(preco)
-        else:
-            return self.book_execucao.getVolumeOperacao(preco)
+        return self.book_execucao.getVolumeOperacao(preco)
         
     def temSaldoLiquido(self, valor):
-        if (self.filtrar_operacao_curva_capital):
-            return self.book_referencia.temSaldoLiquido(valor)
-        else:
-            return self.book_execucao.temSaldoLiquido(valor)
+        return self.book_execucao.temSaldoLiquido(valor)
             
     def getQuantidadePosicoesAbertas(self):
-        if (self.filtrar_operacao_curva_capital):
-            return self.book_referencia.getQuantidadePosicoesAbertas()
-        else:
-            return self.book_execucao.getQuantidadePosicoesAbertas()
-        
-    def getResultadoPosicoesAbertas(self, data):
-        if (self.filtrar_operacao_curva_capital):
-            return self.book_referencia.getResultadoPosicoesAbertas(data)
-        else:
-            return self.book_execucao.getResultadoPosicoesAbertas(data)
-           
+        return self.book_execucao.getQuantidadePosicoesAbertas()
+                  
     def arredondar_casas_decimais(self, casas=2):
         self.book_execucao.arredondar_casas_decimais(casas)
-        if (self.filtrar_operacao_curva_capital):
-            self.book_referencia.arredondar_casas_decimais(casas)
+        self.book_referencia.arredondar_casas_decimais(casas)
+
+    def atualizar_patrimonio_resultado_posicoes_abertas(self, pregao):
+        self.book_referencia.atualizarPatrimonio(pregao, 'INC_CAPITAL', self.book_referencia.getResultadoPosicoesAbertas(pregao))
+        self.book_execucao.atualizarPatrimonio(pregao, 'INC_CAPITAL', self.book_execucao.getResultadoPosicoesAbertas(pregao))
